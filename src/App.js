a960e4157ab3134931c6baa2986317fe28a91fcf
Ashley Chang <ashley_e_chang@brown.edu>
2020-12-05 21:53:54 -0500

    add credits in html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+function toRow(rowData, num) {
+    const a = rowData['data'][num]
+    console.log(a)
+    return(
+        <div className={"genre row"}>
+            <div className={"data column"}>{a['Characters']}</div>
+             <div className={"data column"}>{a['Form']}</div>
+            <div className={"data column"}>{a['Genre']}</div>
+        </div>
+    )
+}
+
+const Header = <div className={"row"}>
+        <h3 className={"column"}>{"Characters"}</h3>
+        <h3 className={"column"}>{"Form"}</h3>
+        <h3 className={"column"}>{"Genre"}</h3>
+    </div>
+
+
+function Randoms (data) {
+    return(
+        <div>
+            {Header}
+            <div>{toRow(data, 0)}</div>
+            <div>{toRow(data, 1)}</div>
+            <div>{toRow(data, 2)}</div>
+            <div>{toRow(data, 3)}</div>
+            <div>{toRow(data, 4)}</div>
+            <div>{toRow(data, 5)}</div>
+        </div>
+    );
+}
+
 
 function App() {
+
+const [genres, setGenres] = useState(0);
+
+  function fetchGenres() {
+    fetch('https://ursas-backend.herokuapp.com/random_genre')
+        .then(response => response.json())
+        .then(data => setGenres(data));
+  }
+  let table = <div/>
+  if(genres !== 0) {
+    table = <Randoms data={genres}/>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"core"}>
+      <h1>Random Genre Generator</h1>
+      <button onClick={() => fetchGenres()}>Click to Vibe</button>
+      {table}
     </div>
   );
 }
